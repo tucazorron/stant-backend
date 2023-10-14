a80dc3e2052aca6e8a8235b6407207f31863cde6
artur zorron <tucazorron@gmail.com>
2023-10-14 18:03:36 -0300

    defines postgres as database

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2023_10_14_144110) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "talks", force: :cascade do |t|
     t.string "title"
     t.integer "duration"
